57d23a6c0c7df0194ac473b1c9bc334601fe0d6b
Victoria-Gudoit <vgudojt@mail.ru>
2022-03-03 04:20:37 +0300

    fix(TR-28): fix bug in modalWarning.js and add changes in module.scss and index.html

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-import { btnDeleteAll, btnCancelModal, modalDeleteAll } from "./modal.js";
+import { modalWarning } from "./components/ModalWarning.js";
 import { currentTime } from "./components/Clock.js";
 import { btnAdd } from "./components/ModalToDo.js";
 import { toast } from "./components/Toast.js";
