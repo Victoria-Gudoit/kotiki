9fd7357aef2838288f2dc8900cf78b2edb875cd0
Elena-Kovalevskaya <elena.kovalevskaya6@gmail.com>
2022-03-06 16:48:26 +0300

    refactor(TR-35): change structure localstorageAPI.js:create object with keys and methods

--- src/scripts/services/localStorageAPI.js
+++ src/scripts/services/localStorageAPI.js
@@ -1,32 +1,23 @@
-const TODO_TASK_KEY = "todos-data";
+const BASE_SERVISE = {
+  keys: {
+    TODO_TASK_KEY: "todos-data",
+    IN_PROGRESS_TASK_KEY: "in-progress-data",
+  },
+  setTodosData(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+  },
+  getTodosData(key) {
+    let data = JSON.parse(localStorage.getItem(key));
+    return (data ??= []);
+  },
+  setTodosInProgressData(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+  },
 
-function setTodosData(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
-}
-
-function getTodosData(key) {
-  let data = JSON.parse(localStorage.getItem(key));
-  return (data ??= []);
-}
-
-////////////////////
-
-const IN_PROGRESS_TASK_KEY = "in-progress-data";
-
-function setTodosInProgressData(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
-}
-
-function getTodosInProgressData(key) {
-  let data = JSON.parse(localStorage.getItem(key));
-  return (data ??= []);
-}
-
-export {
-  TODO_TASK_KEY,
-  setTodosData,
-  getTodosData,
-  IN_PROGRESS_TASK_KEY,
-  setTodosInProgressData,
-  getTodosInProgressData,
+  getTodosInProgressData(key) {
+    let data = JSON.parse(localStorage.getItem(key));
+    return (data ??= []);
+  },
 };
+
+export { BASE_SERVISE };
