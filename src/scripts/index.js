260935622d37c6c1ee2a5dd3be09a6a15c938523
Victoria-Gudoit <vgudojt@mail.ru>
2022-03-10 23:00:32 +0300

    refactor(TR-51): change structer in index.html, components, config and index.scss

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,7 @@ import { currentTime } from "./components/Clock.js";
 import { modalToDo } from "./components/ModalToDo.js";
 import { toast } from "./components/Toast.js";
 import { counter } from "./components/Counter.js";
-import { handleUsers } from "./services/mockAPI.js";
+import { handleGetUsers } from "./services/mockAPI.js";
 import { initCard } from "./components/Card.js";
 import { printUsersInModal } from "./services/placeholderAPI";
 
@@ -14,5 +14,5 @@ function initApp() {
   currentTime();
   modalToDo.init();
   printUsersInModal();
-  handleUsers();
+  handleGetUsers();
 }
