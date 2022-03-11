0570c978b38128b821d4e023893279df066c50ac
Victoria-Gudoit <vgudojt@mail.ru>
2022-03-11 03:23:32 +0300

    fix(TR-56): fix function ColumnTodos in Counter.js

--- src/scripts/components/Counter.js
+++ src/scripts/components/Counter.js
@@ -1,6 +1,8 @@
 import { BASE_SERVISE } from "../services/localStorageAPI.js";
 
-const counter = new ColumnTodos();
+const columnTodo = new ColumnTodos();
+const columnProgress = new ColumnTodos();
+const columnDone = new ColumnTodos();
 
 function ColumnTodos() {
   this.cardsInColumnTodo = function () {
@@ -8,7 +10,28 @@ function ColumnTodos() {
     const counterItem = document.querySelector("#counter-todo");
     const amountOfCards = tasks.length;
     counterItem.textContent = amountOfCards;
+    if (!amountOfCards) {
+      counterItem.textContent = "";
+    }
+  };
+  this.cardsInColumnProgress = function () {
+    const tasks = BASE_SERVISE.getTodosInProgress();
+    const counterItem = document.querySelector("#counter-progress");
+    const amountOfCards = tasks.length;
+    counterItem.textContent = amountOfCards;
+    if (!amountOfCards) {
+      counterItem.textContent = "";
+    }
+  };
+  this.cardsInColumnDone = function () {
+    const tasks = BASE_SERVISE.getTodosInColumnDone();
+    const counterItem = document.querySelector("#counter-done");
+    const amountOfCards = tasks.length;
+    counterItem.textContent = amountOfCards;
+    if (!amountOfCards) {
+      counterItem.textContent = "";
+    }
   };
 }
 
-export { counter };
+export { columnTodo, columnProgress, columnDone };
